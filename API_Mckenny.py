39cf17d5f666725d3f14873a037474c8cccc7f6e
Emad A <emadaljabry@babylon-2.local>
2022-10-02 10:10:37 -0400

    using python for requests

---
+++ API_Mckenny.py
@@ -0,0 +1,204 @@
+
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/binaryValue/25/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/binaryValue/26/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/binaryValue/27/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/binaryValue/28/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/binaryValue/29/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/binaryValue/30/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/analogValue/25/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify= False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/analogValue/26/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/analogValue/27/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/analogValue/28/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/analogValue/29/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/analogValue/30/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=1nf3d32b74ekp1fvq6xvz85pfy'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+import requests
+
+url = "https://10.50.241.5/api/rest/v1/protocols/bacnet/local/objects/multistateValue/5/properties/presentValue"
+
+payload = ""
+headers = {
+  'Authorization': 'Basic dGVhbTU6Q2hhbGxlbmdlVGVhbTU=',
+  'Cookie': 'ECLYPSERESTSESSIONID=12mqmkw373sx7lik73e7xzk4s'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+
+print(response.text)
+
+
+json_result = response.json()
+# requests < 1.0: json_result = result.json
+
+json_result['value'].update(response.json()['value'])
+
+
+
